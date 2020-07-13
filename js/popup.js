888f09140a6cf94972969eb3b547aa63d8d8df7e
Srijan R Shetty <srijan.shetty@gmail.com>
2020-07-13 23:10:38 +0530

    feat: first commit

---
+++ js/popup.js
@@ -0,0 +1,23 @@
+const debounce = (func, wait = 100) => {
+  let timeout;
+  return function(...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func.apply(this, args);
+    }, wait);
+  };
+}
+
+const convert = (value, rate) => Number(value * rate).toFixed(2);
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const res = await fetch("https://api.exchangeratesapi.io/latest?symbols=INR&base=USD")
+  const data = await res.json();
+  const rate = 10 / data["rates"]["INR"];
+
+  const source= document.getElementById('source');
+  const target = document.getElementById('target');
+  source.onkeydown = debounce(event => {
+    target.innerHTML = convert(Number(event.target.value), rate);
+  });
+}, false);
